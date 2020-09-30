3f6d8e9c0e81510f62433b25d3505eaadfc603e4
Your Name <prateekpriyesh@gmail.com>
2020-09-30 15:48:47 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput, Appbar, Button } from "react-native-paper";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    fname: "",
+    sname: ""
+  };
+
+  submitit() {
+    fetch("https://love-calculator.p.rapidapi.com/getPercentage")
+      .then(data => data.json())
+      .then(data2 => {
+        console.log(data2);
+      });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar.Header>
+          <Appbar.Content
+            title="Love % Calculator"
+            style={{ alignItems: "center" }}
+          />
+        </Appbar.Header>
+        <TextInput
+          label="male"
+          value={this.state.fname}
+          onChangeText={text => setText(text)}
+        />
+        <TextInput
+          label="female"
+          value={this.state.sname}
+          onChangeText={text => setText(text)}
+        />
+        <Button
+          icon="cards-heart"
+          mode="contained"
+          style={{ margin: 10 }}
+          onPress={this.submitit.bind(this)}
+        >
+          Calculate
+        </Button>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff"
+  }
 });
+export default App;
